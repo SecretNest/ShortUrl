ab0299b5df8a7b473b111e0f17d50491b0a37e91
Iris Sakura <sgjsakura@hotmail.com>
2019-12-16 15:42:10 +0800

    Update

--- src/OtherResult.cs
+++ src/OtherResult.cs
@@ -3,85 +3,92 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Serialization;
 
 namespace SecretNest.ShortUrl
 {
-    public abstract class OtherResult
-    {
-        public abstract bool HasContent { get; }
-        public abstract int StatusCode { get; }
-        public abstract string Context { get; }
-        public abstract string ContentType { get; }
-    }
+	public abstract class OtherResult
+	{
+		public abstract bool HasContent { get; }
+		public abstract int StatusCode { get; }
+		public abstract string Context { get; }
+		public abstract string ContentType { get; }
+	}
 
-    public abstract class NoContentResult : OtherResult
-    {
-        public override bool HasContent => false;
-        public override string Context => throw new NotSupportedException();
-        public override string ContentType => throw new NotSupportedException();
-    }
+	public abstract class NoContentResult : OtherResult
+	{
+		public override bool HasContent => false;
+		public override string Context => throw new NotSupportedException();
+		public override string ContentType => throw new NotSupportedException();
+	}
 
-    public class Status204Result : NoContentResult
-    {
-        public override int StatusCode => 204;
-    }
+	public class Status204Result : NoContentResult
+	{
+		public override int StatusCode => 204;
+	}
 
-    public class Status205Result : NoContentResult
-    {
-        public override int StatusCode => 205;
-    }
+	public class Status205Result : NoContentResult
+	{
+		public override int StatusCode => 205;
+	}
 
-    public class Status406Result : NoContentResult
-    {
-        public override int StatusCode => 406;
-    }
+	public class Status406Result : NoContentResult
+	{
+		public override int StatusCode => 406;
+	}
 
-    public class Status409Result : NoContentResult
-    {
-        public override int StatusCode => 409;
-    }
+	public class Status409Result : NoContentResult
+	{
+		public override int StatusCode => 409;
+	}
 
-    public class Status410Result : NoContentResult
-    {
-        public override int StatusCode => 410;
-    }
-    
-    public class Status500Result : NoContentResult
-    {
-        public override int StatusCode => 500;
-    }
+	public class Status410Result : NoContentResult
+	{
+		public override int StatusCode => 410;
+	}
 
-    public abstract class ContentResult : OtherResult
-    {
-        public override bool HasContent => true;
-    }
+	public class Status500Result : NoContentResult
+	{
+		public override int StatusCode => 500;
+	}
 
-    public class Status200Result : ContentResult
-    {
-        public override int StatusCode => 200;
-        public override string Context => context;
-        public override string ContentType => contentType;
-        string context, contentType;
+	public abstract class ContentResult : OtherResult
+	{
+		public override bool HasContent => true;
+	}
 
-        protected Status200Result() { }
-        public Status200Result(string context, string contentType)
-        {
-            this.context = context;
-            this.contentType = contentType;
-        }
-    }
+	public class Status200Result : ContentResult
+	{
+		public override int StatusCode => 200;
+		public override string Context => context;
+		public override string ContentType => contentType;
+		string context, contentType;
 
-    public class Status200Result<T> : Status200Result
-    {
-        public override string Context => JsonConvert.SerializeObject(context);
-        public override string ContentType => "application/json";
+		protected Status200Result() { }
+		public Status200Result(string context, string contentType)
+		{
+			this.context = context;
+			this.contentType = contentType;
+		}
+	}
 
-        T context;
+	public class Status200Result<T> : Status200Result
+	{
+		public override string Context => JsonConvert.SerializeObject(context, new JsonSerializerSettings
+		{
+			ContractResolver = new DefaultContractResolver
+			{
+				NamingStrategy = new CamelCaseNamingStrategy()
+			}
+		});
+		public override string ContentType => "application/json";
 
-        public Status200Result(T context)
-        {
-            this.context = context;
-        }
-    }
+		T context;
+
+		public Status200Result(T context)
+		{
+			this.context = context;
+		}
+	}
 
 }
